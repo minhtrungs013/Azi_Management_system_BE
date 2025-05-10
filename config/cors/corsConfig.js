468cc22ec51a79efc500ef88c24151276555f480
minhtrungs013 <trungs0132@gmail.com>
2025-05-11 00:26:57 +0700

    add cors

--- config/cors/corsConfig.js
+++ config/cors/corsConfig.js
@@ -2,7 +2,7 @@ const logger = require('../logger/logger');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        const allowedOrigins = ['http://localhost:3000','http://localhost:5500']; // Các client (domain) được phép truy cập
+        const allowedOrigins = ['http://localhost:3000','http://localhost:5500','https://azi-management-system.vercel.app']; // Các client (domain) được phép truy cập
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             // Nếu không có origin hoặc origin nằm trong danh sách cho phép
             callback(null, true);
