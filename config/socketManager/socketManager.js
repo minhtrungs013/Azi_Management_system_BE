cbc6c7a7c430e6a09658d6cd7c3e190929f5ce36
minhtrungs013 <trungs0132@gmail.com>
2025-06-09 00:08:50 +0700

    call group

--- config/socketManager/socketManager.js
+++ config/socketManager/socketManager.js
@@ -3,8 +3,6 @@ const { getProjectIds } = require('../../controllers/projectController'); // Đi
 const socketManager = (io) => {
     const users = {}; // { username: socketId }
     io.on('connection', (socket) => {
-        console.log(`User connected: ${socket.id}`);
-      
         socket.on('joinGroupChat', ({ group }) => {
             socket.join(group);
             console.log(`User joined group: ${group}`);
@@ -34,14 +32,14 @@ const socketManager = (io) => {
             users[username] = socket.id;
             console.log(`User registered: ${username} (${socket.id})`);
             console.log(users);
-            
+
         });
 
         socket.on('callUser', ({ to, signalData, from, name }) => {
             const targetSocketId = users[to];
-            
+
             console.log(`Call from ${from} to ${to}: ${targetSocketId}`);
-            
+
             if (targetSocketId) {
                 io.to(targetSocketId).emit('callIncoming', {
                     from: name,
@@ -55,7 +53,7 @@ const socketManager = (io) => {
         socket.on('answerCall', ({ to, signal }) => {
             const targetSocketId = users[to];
             console.log(targetSocketId);
-            
+
             if (targetSocketId) {
                 io.to(targetSocketId).emit('callAccepted', {
                     signal,
@@ -66,7 +64,7 @@ const socketManager = (io) => {
 
         socket.on('ice-candidate', ({ to, candidate }) => {
             let targetSocketId = users[to];
-            
+
             if (targetSocketId) {
                 io.to(targetSocketId).emit('ice-candidate', {
                     candidate,
@@ -76,7 +74,7 @@ const socketManager = (io) => {
         socket.on('tesst', ({ to, candidate }) => {
             let targetSocketId = users[to];
             console.log(to);
-            
+
             if (targetSocketId) {
                 io.to(targetSocketId).emit('ice-candidate', {
                     candidate,
@@ -89,6 +87,51 @@ const socketManager = (io) => {
             if (targetSocketId) {
                 io.to(targetSocketId).emit('callEnded');
             }
+        });
+
+        socket.on('groupCall', ({ groupId, from, signalData }) => {
+            const members = io.sockets.adapter.rooms.get(groupId);
+
+            if (!members) return;
+
+            io.to(groupId).except(socket.id).emit('groupCallIncoming', {
+                from,
+                signal: signalData,
+                socketId: socket.id,
+            });
+
+            console.log(`${from} is calling group ${groupId}`);
+        });
+
+        socket.on('groupAnswer', ({ toSocketId, signal }) => {
+            io.to(toSocketId).emit('groupCallAccepted', {
+                from: socket.id,
+                signal,
+            });
+
+            console.log(`User ${socket.id} answered group call from ${toSocketId}`);
+        });
+
+        socket.on('groupIceCandidate', ({ groupId, candidate }) => {
+            io.to(groupId).emit('groupIceCandidate', {
+                from: socket.id,
+                candidate,
+            });
+        });
+
+        socket.on('endGroupCall', ({ group }) => {
+            const room = io.sockets.adapter.rooms.get(group);
+            if (!room) return;
+
+            room.forEach((memberSocketId) => {
+                if (memberSocketId !== socket.id) {
+                    io.to(memberSocketId).emit('groupCallEnded', {
+                        from: socket.id,
+                    });
+                }
+            });
+
+            console.log(`Group call ended in group ${group}`);
         });
 
         socket.on('disconnect', () => {
